9c5de7ea514198bfa0474d00eeab253a3b060d76
koi-as <alextseidensticker@gmail.com>
2025-09-16 19:51:43 -0400

    Using some default code from tkinter documentation to test features

--- start.py
+++ start.py
@@ -1,8 +1,16 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 
-win=tk.Tk()
-win.geometry('750x750')
+win=tk.Tk() # this creates a "top level widget" which is used to create a main window for an application
+win.geometry('750x750') # this sets the height and width of the window
 win.title('Listless')
 
-win.mainloop()
+# win.mainloop()
+
+root=Tk()
+frm=ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text='Hello World').grid(column=0, row=0)
+ttk.Button(frm, text='Quit', command=root.destroy).grid(column=1, row=0)
+root.mainloop()
