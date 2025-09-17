4b273ca2f1c06406707da052a7e341dc7379a9c7
koi-as <alextseidensticker@gmail.com>
2025-09-16 21:03:04 -0400

    Made yes and no buttons tied to different functions

--- start.py
+++ start.py
@@ -6,12 +6,23 @@ from tkinter import ttk
 
 win=tk.Tk()
 frm=ttk.Frame(win, padding=20)
-frm.grid()
+# frm.grid()
 # first value is width, second is height
 win.geometry('1200x600')
 win.title('Listless')
 print(win.configure())
 
-Button(win, text='DIE, loser', command=win.destroy).place(relx=.5, rely=.5, anchor=CENTER)
+prompt=StringVar()
+
+def yesResponse():
+    # some code
+    prompt.set('Prompt works')
+
+def noResponse():
+    prompt.set('Prompt is not working lol')
+
+Label(win, textvariable=prompt, width=40, font=('Courier', 40)).place(relx=.5, rely=.3, anchor=CENTER)
+Button(win, text='YES', font=('Ubuntu', 25), command=yesResponse, bg='light green').place(relx=.4, rely=.5, anchor=CENTER)
+Button(win, text='NO', font=('Ubuntu', 25), command=noResponse, bg='red').place(relx=.6, rely=.5, anchor=CENTER)
 
 win.mainloop()
